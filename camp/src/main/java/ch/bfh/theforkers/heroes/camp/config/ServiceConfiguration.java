c8dfa0e742c7f67b647bc15cc160ec16a6c5458b
wirtj1 <joy.wirth@mafix.ch>
2019-11-21 18:01:01 +0100

    tried to fix Tests but failed

--- camp/src/main/java/ch/bfh/theforkers/heroes/camp/config/ServiceConfiguration.java
+++ camp/src/main/java/ch/bfh/theforkers/heroes/camp/config/ServiceConfiguration.java
@@ -6,7 +6,6 @@ import ch.bfh.theforkers.heroes.camp.service.HeroService;
 import ch.bfh.theforkers.heroes.camp.service.PartyService;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
-import cz.jirutka.spring.embedmongo.EmbeddedMongoBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
@@ -16,33 +15,35 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import java.io.IOException;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "ch.bfh.theforkers.heroes.camp.repo")
+//@EnableMongoRepositories(basePackages = "ch.bfh.theforkers.heroes.camp.repo")
 public class ServiceConfiguration {
-	@Bean
-	public HeroService heroService() {
-		return new EpicHeroService();
-	}
+    @Bean
+    public HeroService heroService() {
+        return new EpicHeroService();
+    }
 
-	@Bean
-	public PartyService campService(HeroService heroService) {
-		return new GreatPartyService(heroService);
-	}
+    @Bean
+    public PartyService campService(HeroService heroService) {
+        return new GreatPartyService(heroService);
+    }
 
-	@Bean
-	@DependsOn("mongo")
-	public MongoClient mongoClient() {
-	return new MongoClient("localhost");
-	}
-	@Bean
-	public MongoTemplate mongoTemplate() {
-	return new MongoTemplate(mongoClient(), "myDatabase");
-	}
-	@Bean(destroyMethod="close")
-	public Mongo mongo() throws IOException {
-	return new EmbeddedMongoBuilder()
-	.version("3.4.17")
-	.bindIp("localhost")
-	.port(27017)
-	.build();
-	}
+//    @Bean
+//    @DependsOn("mongo")
+//    public MongoClient mongoClient() {
+//        return new MongoClient("localhost");
+//    }
+//
+//    @Bean
+//    public MongoTemplate mongoTemplate() {
+//        return new MongoTemplate(mongoClient(), "myDatabase");
+//    }
+//
+//    @Bean(destroyMethod = "close")
+//    public Mongo mongo() throws IOException {
+//        return new EmbeddedMongoBuilder()
+//                .version("3.4.17")
+//                .bindIp("localhost")
+//                .port(27017)
+//                .build();
+//    }
 }
--- camp/src/test/java/ch/bfh/theforkers/heroes/camp/ApplicationTests.java
+++ camp/src/test/java/ch/bfh/theforkers/heroes/camp/ApplicationTests.java
@@ -1,10 +1,13 @@
 package ch.bfh.theforkers.heroes.camp;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	void contextLoads() {
